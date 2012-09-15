6529a1d18d1c8b0826be3e7474d43ef48f61723d
Greg Terrono <gterrono@seas.upenn.edu>
2012-09-15 03:13:47 -0400

    Authentication and saving of token works

--- src/com/terrono/foursquare/FimActivity.java
+++ src/com/terrono/foursquare/FimActivity.java
@@ -1,7 +1,13 @@
 package com.terrono.foursquare;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 
 public class FimActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +15,20 @@ public class FimActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button logInBtn = (Button)findViewById(R.id.log_in);
+
+		SharedPreferences settings = getSharedPreferences(FimWebView.FILE_NAME, 0);
+		boolean authenticated = settings.getBoolean(FimWebView.AUTHENTICATED, false);
+		
+		if(authenticated)
+			logInBtn.setText("Logged In");
+		else
+	        logInBtn.setOnClickListener(new OnClickListener() {
+	        	public void onClick(View v) {
+	        		Intent intent = new Intent(FimActivity.this, FimWebView.class);
+	        		startActivity(intent);
+	        	}
+	        });
     }
 }
